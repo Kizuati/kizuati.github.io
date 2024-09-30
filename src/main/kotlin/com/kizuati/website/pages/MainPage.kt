40b461b57bf3e9022dd848f13aa286a2a0aba27b
SilverAndro <52360088+SilverAndro@users.noreply.github.com>
2024-09-30 14:46:42 -0400

    redesign and random videos

--- src/main/kotlin/com/kizuati/website/pages/MainPage.kt
+++ src/main/kotlin/com/kizuati/website/pages/MainPage.kt
@@ -15,44 +15,30 @@ object MainPage : Page() {
     }
 
     override fun BODY.body() {
-        split(Justify.CENTER, "heading") {
-            img("Kizu's profile picture", "images/pfp.png") {
-                id = "profile_picture"
-                width = 400.px
-                height = 400.px
-            }
-            div {
-                id = "bio"
-                article {
-                    h1 { +"Hi,I'm Kizu" }
-                    h2 { +"I edit videos and like to waffle about game design." }
-                    +"Those videos range from game design deep dives to stream highlights with anything in between."
-                    br2
-                    +"If you want your videos edited - hit me up,we'll work something out :)"
+        split(Justify.SPACE_EVENLY, "heading") {
+            split(Justify.LEFT, "combo") {
+                img("Kizu's profile picture", "images/pfp.png") {
+                    id = "profile_picture"
+                    width = 400.px
+                    height = 400.px
+                    style = "align-self: flex-start;"
+                }
+                div {
+                    id = "bio"
+                    article {
+                        h1 { +"Hi,I'm Kizu" }
+                        h2 { +"I edit videos and like to waffle about game design." }
+                        +"Those videos range from game design deep dives to stream highlights with anything in between."
+                        br2
+                        +"If you want your videos edited - hit me up,we'll work something out :)"
+                    }
                 }
             }
-        }
-
-
-        div("split content") {
-            style = "justify-content: ${Justify.SPACE_EVENLY.css}"
-
-            // Best of Helios || Returning To Billy
-            // Best of Helios || July
-            div {
-                videos(
-                    "VhLVh5mauu4",
-                    "yyc-o4DJHic",
-                    "These are some of the stream highlights I've done recently. Pacing,SFX,Music,etc. - all at the client's discretion."
-                )
-            }
-
             div("contact") {
                 stack(Justify.SPACE_EVENLY) {
-                    split(Justify.CENTER, "footer") {
-                        div { id = "footer-name"; +"Kizu"; br; +"Axiosov" }
+                    split(Justify.CENTER, "comm-links") {
                         div {
-                            id = "footer-email"
+                            id = "comm-email"
                             +"Want to commission me?"
                             br2
                             email("commissions@kizuati.com")
@@ -66,43 +52,90 @@ object MainPage : Page() {
                     }
                 }
             }
+        }
 
-            // D&D PTB Deep Dive
-            // Castlevania PTB Deep Dive
-            div {
-                videos(
-                    "Ivon9mr6XNE",
-                    "lsEK0gzTCxc",
-                    "There's also a lot of game design focused deep dive videos made by me on DBD that have pretty graphics to accompany my points."
-                )
-            }
+        split(Justify.SPACE_EVENLY) {
+            id = "videos"
+            script { unsafe { +"""
+                let vC = document.getElementById("videos");
+                let videos = {
+                    "VhLVh5mauu4": "Quam at odio iusto omnis animi fugit. Tempore qui sapiente nulla animi. Et aperiam et eveniet repellat unde ea.",
+                    "yyc-o4DJHic": "Quam at odio iusto omnis animi fugit. Tempore qui sapiente nulla animi. Et aperiam et eveniet repellat unde ea.",
+                    "Ivon9mr6XNE": "Quam at odio iusto omnis animi fugit. Tempore qui sapiente nulla animi. Et aperiam et eveniet repellat unde ea.",
+                    "lsEK0gzTCxc": "Quam at odio iusto omnis animi fugit. Tempore qui sapiente nulla animi. Et aperiam et eveniet repellat unde ea."
+                }
+                let put = [];
+                var randomID = function (obj) {
+                    var keys = Object.keys(obj);
+                    return keys[ keys.length * Math.random() << 0];
+                };
+                let insertVideo = function(id, desc) {
+                    const stack = document.createElement("div");
+                    const descDiv = document.createElement("div");
+                    const videoContainer = document.createElement("div");
+                    const iframe = document.createElement("iframe");
+                    
+                    stack.className = "stack video-stack";
+                    stack.style = "justify-content: center";
+                    
+                    videoContainer.className = "video-container";
+                    
+                    iframe.className = "embed";
+                    iframe.width = "384";
+                    iframe.height = "216";
+                    iframe.title = "YouTube video player";
+                    iframe.src = "https://www.youtube-nocookie.com/embed/" + id;
+                    iframe.setAttribute("frameborder", "0");
+                    iframe.setAttribute("allow", "clipboard-write; encrypted-media; picture-in-picture; web-share");
+                    iframe.setAttribute("allowfullscreen", "1");
+                    iframe.setAttribute("class", "embed");
+                    
+                    descDiv.className = "video-desc";
+                    descDiv.append(desc);
+                    
+                    videoContainer.appendChild(iframe)
+                    stack.appendChild(videoContainer);
+                    vC.appendChild(stack);
+                    stack.appendChild(descDiv);
+                };
+                let insertRandom = function() {
+                    var rK = randomID(videos);
+                    while (put.includes(rK)) { rK = randomID(videos); };
+                    console.log(rK);
+                    put.push(rK);
+                    insertVideo(rK, videos[rK])
+                }
+                insertRandom();
+                insertRandom();
+                insertRandom();
+                insertRandom();
+            """.trimIndent() }}
+        }
+
+        article("content") {
+            +" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra, erat ut accumsan aliquet, eros elit viverra leo, in scelerisque mauris quam tristique diam. Etiam sollicitudin dolor sed ex feugiat pharetra. Quisque id ligula ut erat iaculis suscipit. Mauris vitae tincidunt eros. Curabitur maximus tincidunt elit, nec consectetur elit faucibus vel. Sed venenatis lacinia venenatis. Suspendisse volutpat gravida enim eu suscipit. Aliquam erat volutpat. Aliquam porttitor diam eget varius iaculis. Morbi ac ligula vel elit imperdiet mattis at eu justo. Praesent nec odio enim."
+            br2
+            +"Etiam ullamcorper felis nec sem dignissim, eleifend maximus tortor pellentesque. Morbi eros leo, ultricies et eleifend sed, mollis vitae libero. Sed vehicula mauris vel leo sagittis, vel tincidunt libero suscipit. Nulla dignissim odio lobortis velit maximus pulvinar. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam sollicitudin pulvinar tincidunt. Morbi non vulputate urna. Nulla quis justo nulla. Mauris lobortis elementum sem, id vestibulum nisl placerat ac. "
         }
     }
 
-    fun DIV.videos(idA: String, idB: String, desc: String) {
-        stack(Justify.CENTER) {
-            iframe {
-                width = "480"
-                height = "270"
-                title = "YouTube video player"
-                src = "https://www.youtube-nocookie.com/embed/${idA}"
-                attributes["frameborder"] = "0"
-                attributes["allow"] = "clipboard-write; encrypted-media; picture-in-picture; web-share"
-                attributes["allowfullscreen"] = "1"
-                attributes["class"] = "embed"
+    private fun DIV.video(id: String, desc: String) {
+        stack(Justify.CENTER, "video-stack") {
+            div("video-container") {
+                iframe {
+                    width = "384"
+                    height = "216"
+                    title = "YouTube video player"
+                    src = "https://www.youtube-nocookie.com/embed/${id}"
+                    attributes["frameborder"] = "0"
+                    attributes["allow"] = "clipboard-write; encrypted-media; picture-in-picture; web-share"
+                    attributes["allowfullscreen"] = "1"
+                    attributes["class"] = "embed"
+                }
             }
+
             div("video-desc") {
                 +desc
-            }
-            iframe {
-                width = "480"
-                height = "270"
-                title = "YouTube video player"
-                src = "https://www.youtube-nocookie.com/embed/${idB}"
-                attributes["frameborder"] = "0"
-                attributes["allow"] = "clipboard-write; encrypted-media; picture-in-picture; web-share"
-                attributes["allowfullscreen"] = "1"
-                attributes["class"] = "embed"
             }
         }
     }
@@ -137,12 +170,12 @@ object MainPage : Page() {
                 }
                 
                 .content {
-                    margin-top: 20px;
-                    margin-bottom: 85px;
+                    font-size: 20px;
+                    margin: 2em 2em 70px 2em;
                 }
                 
                 .embed {
-                    animation-duration: 3s;
+                    animation-duration: 2s;
                     animation-name: slidein;
                 }
 
@@ -163,13 +196,15 @@ object MainPage : Page() {
                     width: 480px;
                 }
                 
-                #footer-name {
-                    text-align: center;
-                    font-size: 30px;
-                    margin-right: 20px;
+                .comm-links {
+                    padding-bottom: 2em;
                 }
                 
-                #footer-email {
+                .combo {
+                    width: 60%;
+                }
+                
+                #comm-email {
                     text-align: center;
                     font-size: 20px;
                 }
@@ -184,7 +219,7 @@ object MainPage : Page() {
                 #bio {
                     font-size: 22px;
                     line-height: 1.3;
-                    width: 40%;
+                    width: 100%;
 
                     display: flex;
                     align-items: center;
@@ -206,6 +241,28 @@ object MainPage : Page() {
                 a.nav {
                     align-self: center;
                 }
+                
+                .video-stack {
+                    width: 22%;
+                }
+                
+                .video-desc {
+                    width:100%;
+                }
+                
+                .video-container {
+                    position: relative;
+                    width: 100%;
+                    height: 0;
+                    padding-bottom: 56.25%;
+                }
+                .embed {
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100%;
+                }
             """.trimIndent()
         }
 
@@ -216,14 +273,6 @@ object MainPage : Page() {
             
             #bio {
                 width: 70%;
-            }
-            
-            .split.content {
-                flex-direction: column;
-            }
-            
-            .split.footer {
-                margin-bottom: 40px;
             }
         """.trimIndent()
 
