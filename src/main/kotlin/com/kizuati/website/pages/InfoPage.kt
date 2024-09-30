aafb75c8694276e953441897511a44434ca4e171
Kizu Axiosov <73559305+Kizuati@users.noreply.github.com>
2024-10-01 01:18:03 +0300

    raise rates,again

--- src/main/kotlin/com/kizuati/website/pages/InfoPage.kt
+++ src/main/kotlin/com/kizuati/website/pages/InfoPage.kt
@@ -29,9 +29,9 @@ object InfoPage : Page() {
                 article {
                     h1 { +"All You Oughta Know" }
                     h2 { +"You can find my pricing & all the other important comm deets here." }
-                    +"I go for about 9\$ per hour of work on a video,so for example,me working for 6 hours on a video of yours will cost you a nice 54\$. "
+                    +"I go for about 10\$ per hour of work on a video,so for example,me working for 6 hours on a video of yours will cost you a nice 60\$. "
                     br2
-                    +"If you want something especially fancy,my per hour rate can go up to 10-12\$ and you will be notified of that depending on the material,etc."
+                    +"If you want something especially fancy,my per hour rate can go up to 12-14\$ and you will be notified of that depending on the material,etc."
                 }
             }
         }
