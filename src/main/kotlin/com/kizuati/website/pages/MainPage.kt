601e1643b0c18836584a8dce868ac5b4bd4cd0a5
Kizu Axiosov <73559305+Kizuati@users.noreply.github.com>
2025-04-03 16:28:35 +0300

    my original braces that i lost were cheaper than this commit message was to my sanity

--- src/main/kotlin/com/kizuati/website/pages/MainPage.kt
+++ src/main/kotlin/com/kizuati/website/pages/MainPage.kt
@@ -27,7 +27,7 @@ object MainPage : Page() {
                     article {
                         h1 { +"Hi,I'm Kizu." }
                         h2 { +"I'm a semi-professional video maker." }
-			h3 { i {+"That's to say, I edit and write things.") ;
+			h3 { i {+"That's to say, I edit and write things."}} ;
                         +"I've done a lot of things, including standardizing full blown video essays that get made within hours of new content dropping..."
 			br2
                         +"...All the way to scrubbing through 24h of VOD footage to construct a highlights reel while sick to the core."
